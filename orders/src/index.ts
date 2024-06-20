0a7fdcc38b18173a18c678f1534f284e9009549d
Fran <hopeli919@gmail.com>
2024-06-20 15:00:02 -0700

    added startup msg to orders srv

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -7,6 +7,7 @@ import { ExpirationComplteListner } from "./events/listeners/expiration-complete
 import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
 
 const start = async () => {
+    console.log("Starting...");
     // check env variable is defined
     if (!process.env.JWT_KEY) {
         throw new Error("JWT_KEY must be defined");
